36f34d3a3d4d6009f02d710599806a926c041dcf
tamilmaranr <108916935+tamilmaranr@users.noreply.github.com>
2023-03-28 21:46:33 +0530

    Add files via upload

---
+++ Image.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ShelbyGT500 from './Shelby GT500.jpeg'
+
+function Image() {
+  return (
+    <div className='div1'>
+        <center>
+            <img src={ShelbyGT500} width="800px" hieght="350px" alt=''></img>
+        </center>
+    </div>
+  )
+}
+
+export default Image
